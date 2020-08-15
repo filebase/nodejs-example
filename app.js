6b26c943f571ceb18c4947526d2998f253c64470
Filebase <hello@filebase.com>
2020-08-15 09:59:22 -0400

    Update example to use PutObject

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ s3.listBuckets(function(err, data) {
   if (err) {
     console.log(err, err.stack);
   } else {
-    console.log(data);
+    var params = {
+      Body: 'Hello, world!',
+      Bucket: data['Buckets'][0]['Name'],
+      Key: 'exampleobject',
+      ContentType: 'text/plain',
+      Metadata: {
+       'metadata1': 'value1',
+       'metadata2': 'value2'
+      }
+    };
+    s3.putObject(params, function(err, data) {
+      if (err) {
+        console.log(err, err.stack);
+      } else {
+        console.log(data);
+      }
+    });
   }
 });
